e5fecbc2b5f1dd34b6b7f1d11b8de3f75a809db4
r0b3rth4ns3n <r0b3rth4ns3n@protonmail.com>
2022-10-24 17:41:33 +0200

    leaflet

---
+++ src/lib/Map.svelte
@@ -0,0 +1,37 @@
+<script>
+
+    import { onMount,onDestroy,setContext } from 'svelte';
+    import { map,tileLayer } from 'leaflet';
+    
+    setContext('leaflet',{ getMap : () => MAP });
+
+    // export let lat;
+	// export let lng;
+	// export let zoom;
+
+    let container;
+    let MAP;
+
+    onMount(async () => {
+        MAP = map(container).setView([51.505, -0.09], 4);
+        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(MAP);
+    });
+
+    onDestroy(async () => { if (MAP) MAP.remove(); });
+
+</script>
+
+<main>
+    <div bind:this={container}>
+        {#if MAP}
+            <slot/>
+        {/if}
+    </div>
+</main>
+
+<style>
+    @import 'leaflet/dist/leaflet.css';
+    main div {
+        height: 800px;
+    }
+</style>
---
+++ src/lib/Marker.svelte
@@ -0,0 +1,14 @@
+<script>
+
+    import { getContext } from 'svelte';
+	import { marker } from 'leaflet';
+
+	const { getMap } = getContext('leaflet');
+	const map = getMap();
+
+	export let lat;
+	export let lng;
+
+	marker([lat,lng]).addTo(map);
+
+</script>
