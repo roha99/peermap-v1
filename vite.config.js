834dc470fec050857df06f4cf9c6558403ab7b2e
r0b3rth4ns3n <r0b3rth4ns3n@protonmail.com>
2022-11-11 15:40:20 +0100

    progressive web app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    // https://vite-pwa-org.netlify.app/
+    VitePWA({ registerType: 'autoUpdate' }),
+  ]
 })
